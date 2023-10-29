d39b966a093ee1512a76bd3c539a4917c543fa64
waqaarali <109590536+waqaarali@users.noreply.github.com>
2023-10-29 21:32:43 +0000

    Latest changes

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <div className="fixed top-0 left-0 w-full h-[70px] py-5 px-24">
+      <div className="fixed top-0 left-0 w-full h-[70px] py-5 px-10 sm:px-24">
         <img src={logo} className="relative w-max h-full" alt="CUDE" />
       </div>
 
-      <div className="flex items-center justify-end w-full h-full">
+      <div className="flex items-end sm:items-center justify-end w-full h-full">
         <div className="MainBody flex flex-col justify-center gap-5 bg-white rounded-sm py-10 px-24">
-          <h3 className="MainHeading text-xl sm:text-4xl">
+          <h3 className="MainHeading text-2xl sm:text-4xl">
             We are now live...
           </h3>
           <p className="MainInfo text-sm sm:text-lg">
-            Join our testing partners today<br/>and gain early bird access to our<br/>solution for free.
+            Join our testing partners today and gain early bird access to our solution for free.
           </p>
           <a className="MainInfo text-sm rounded-sm bg-[#0065FF] hover:bg-[#0065FF]/50 transition text-white p-2 text-center" href="https://prototype.thecude.com/">
             Register for free
           </a>
         </div>
+
+        <a className="MainInfo text-white fixed left-0 bottom-0 w-full p-2 bg-[#0065FF] hover:bg-[#1e76fd] text-center transition" href="https://prototype.thecude.com/">
+          Code Secure Ltd ©️ 2023 | 14766852
+        </a>
       </div>
     </div>
   );

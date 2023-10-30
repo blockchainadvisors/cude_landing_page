a0e8aefb9743857e9b5390e4545b0f4c0a7c5256
waqaarali <109590536+waqaarali@users.noreply.github.com>
2023-10-30 10:45:36 +0000

    Responsive issue fix & Safari web-browser fix

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
   return (
     <div className="App">
       <div className="fixed top-0 left-0 w-full h-[70px] py-5 px-10 sm:px-24">
-        <img src={logo} className="relative w-max h-full" alt="CUDE" />
+        <img src={logo} className="relative w-[104px] h-[27px]" alt="CUDE" />
       </div>
 
-      <div className="flex items-end sm:items-center justify-end w-full h-full">
-        <div className="MainBody flex flex-col justify-center gap-5 bg-white rounded-sm py-10 px-24">
+      <div className="flex items-end sm:pb-20 lg:items-center justify-end w-full h-full">
+        <div className="MainBody flex flex-col justify-center gap-5 bg-white rounded-sm">
           <h3 className="MainHeading text-2xl sm:text-4xl">
             We are now live...
           </h3>
